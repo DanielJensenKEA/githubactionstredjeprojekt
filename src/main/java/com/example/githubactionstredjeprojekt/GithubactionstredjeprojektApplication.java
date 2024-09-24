a4d8f3c8f3c9fa48b2791f573343af233c152760
DanielJensenKEA <daje0003@stud.kea.dk>
2024-09-24 13:55:18 +0200

    initial commit

---
+++ src/main/java/com/example/githubactionstredjeprojekt/GithubactionstredjeprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstredjeprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionstredjeprojektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionstredjeprojektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionstredjeprojekt/GithubactionstredjeprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionstredjeprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionstredjeprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
